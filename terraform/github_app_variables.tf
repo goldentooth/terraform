986e241baa7edae4ab091f67f541d44b76a9e869
Nate Douglas <ndouglas@users.noreply.github.com>
2025-05-06 17:40:05 -0400

    Factor out common code.

--- terraform/github_app_variables.tf
+++
@@ -1,8 +0,0 @@
-module "github_app_variables" {
-  source = "github.com/bitterbridge/terraform-modules//terraform/modules/github_app_variables?ref=v0.0.3"
-
-  for_each = toset(keys(module.repositories.repositories))
-
-  organization_name = "goldentooth"
-  repository_name   = each.key
-}
--- terraform/repositories.tf
+++
@@ -1,5 +0,0 @@
-module "repositories" {
-  source = "github.com/bitterbridge/terraform-modules//terraform/modules/repositories?ref=v0.0.3"
-
-  organization_name = "goldentooth"
-}
