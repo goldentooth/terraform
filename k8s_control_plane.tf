4088af6d3dd41d8bc0d2a2a461fb476686b5029c
Nate Douglas <ndouglas@users.noreply.github.com>
2025-09-26 14:23:12 -0400

    Adds cp.k8s.goldentooth.net address.

---
+++ k8s_control_plane.tf
@@ -0,0 +1,13 @@
+resource "aws_route53_record" "k8s_control_plane" {
+  zone_id = local.zone_id
+  name    = "cp.k8s.goldentooth.net"
+  type    = "A"
+  ttl     = local.default_ttl
+
+  records = [
+    "10.4.0.10",
+    "10.4.0.11",
+    "10.4.0.12",
+    "10.4.0.13"
+  ]
+}
