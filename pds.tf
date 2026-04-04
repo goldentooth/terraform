7d45cd35d56c845c4fc878a133301fed78394df2
Nate Douglas <ndouglas@users.noreply.github.com>
2026-04-04 09:34:27 -0400

    feat(terraform): add IAM user for PDS S3 backup
    Dedicated IAM user with policy scoped to Put/Get/Delete/List
    on the goldentooth-pds-backup bucket only.

--- pds.tf
+++ pds.tf
@@ -56,3 +56,47 @@ resource "aws_s3_bucket_lifecycle_configuration" "pds_backup" {
     }
   }
 }
+
+# IAM user for PDS backup writes to S3.
+resource "aws_iam_user" "pds_backup" {
+  name = "goldentooth-pds-backup"
+}
+
+resource "aws_iam_user_policy" "pds_backup" {
+  name = "pds-backup-s3-access"
+  user = aws_iam_user.pds_backup.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+        ]
+        Resource = [
+          aws_s3_bucket.pds_backup.arn,
+          "${aws_s3_bucket.pds_backup.arn}/*",
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_access_key" "pds_backup" {
+  user = aws_iam_user.pds_backup.name
+}
+
+output "pds_backup_aws_access_key_id" {
+  description = "AWS access key ID for PDS backup IAM user."
+  value       = aws_iam_access_key.pds_backup.id
+}
+
+output "pds_backup_aws_secret_access_key" {
+  description = "AWS secret access key for PDS backup IAM user."
+  value       = aws_iam_access_key.pds_backup.secret
+  sensitive   = true
+}
